03c3e9c4b6b28e0f9afb7035b4a11602f89fa246
carrac22 <carrac@gwu.edu>
2023-02-04 17:24:31 -0500

    dynamic and nav bar updates

---
+++ restaurant.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+  $(window).scroll(function(){
+      if(this.scrollY > 20){
+          $('.navbar').addClass("sticky");
+      }else{
+          $('.navbar').removeClass("sticky");
+      }
+    });
+
+
+  // toggling menu bar
+  $('.menu-btn').click(function(){
+      $('.navbar .menu').toggleClass("active");
+      $('.menu-btn i').toggleClass("active");
+
+  });
+
+});
+
